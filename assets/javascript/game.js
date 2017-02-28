4bc832c1b591f8e63e5aea4d5cc87be1f0eef75b
John Gorman <johngorm@usc.edu>
2017-02-27 18:30:09 -0600

    Game functional

---
+++ assets/javascript/game.js
@@ -0,0 +1,86 @@
+
+var generateCrystalValue = function(){
+	var value = Math.floor((Math.random() * 12) + 1);
+	return value;
+	
+};
+
+var generateTargetValue = function(){
+	return Math.floor((Math.random() * 101) + 19);
+};
+
+var writeWins
+
+$(document).ready(function(){
+
+
+	var playerScore = 0;
+	var win = 0;
+	var loss = 0;
+	var green_gem = "./assets/images/green_gem.png";
+	var red_gem = "./assets/images/red_gem.png";
+	var yellow_gem ="./assets/images/yellow_gem.png";
+	var purple_gem = "./assets/images/purple_gem.png";
+
+	var gems_pic = [green_gem, red_gem, yellow_gem, purple_gem];
+
+	var writeTargetToScreen = function(){
+		$("#random-goal").html(targetValue);
+	};
+
+	var writePlayerScoreToScreen = function(){
+		$("#score").html(playerScore);
+	};
+
+	var writeWinLossToScreen = function(){
+		$("#win_lose").html("Wins: " + win +"\n\nLosses: " + loss);
+	}
+
+	var resetGame = function(){
+		$("img").each(function(index) {
+			$(this).data('value',generateCrystalValue());
+		});
+		playerScore = 0;
+		writePlayerScoreToScreen();
+		targetValue = generateTargetValue();
+		writeTargetToScreen();
+	};
+
+
+	var targetValue = generateTargetValue();
+	$("#random-goal").html(targetValue);
+	writeWinLossToScreen();
+	
+	for(var ii = 0; ii < gems_pic.length; ii++){
+
+		var img = $("<img width=150 height=100>");
+		img.addClass("crystals-click");
+		img.attr('src', gems_pic[ii]);
+		var val = generateCrystalValue();
+		img.data('value', val);
+		
+		$("#crystals").append(img);
+	}
+
+
+
+	$(".crystals-click").on('click', function(){
+		playerScore += ($(this).data('value'));
+		$("#score").html(playerScore);
+		if(playerScore > targetValue){
+			loss++;
+			console.log(loss);
+			writeWinLossToScreen();
+			resetGame();
+			
+		}
+		else if(playerScore === targetValue){
+			win++;
+			console.log(win);
+			resetGame();
+			writeWinLossToScreen();
+			alert("You win!");
+		}
+	})
+
+});
