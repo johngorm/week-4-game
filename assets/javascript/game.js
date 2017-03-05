192a8fea8dbc2e6ee9879001ff5b83fd11407f30
John Gorman <johngorm@usc.edu>
2017-03-05 16:35:29 -0600

    Implement unique values for crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,6 @@
-
-var generateCrystalValue = function(){
-	var value = Math.floor((Math.random() * 12) + 1);
-	return value;
-	
-};
-
-var generateTargetValue = function(){
-	return Math.floor((Math.random() * 101) + 19);
-};
-
 $(document).ready(function(){
 
-
+	//Initialize variables and get image paths/
 	var playerScore = 0;
 	var win = 0;
 	var loss = 0;
@@ -19,25 +8,52 @@ $(document).ready(function(){
 	var red_gem = "./assets/images/red_gem.png";
 	var yellow_gem ="./assets/images/yellow_gem.png";
 	var purple_gem = "./assets/images/purple_gem.png";
-
-	// var $clickCyrstals = $("crystals-click");
-	
 	var gems_pic = [green_gem, red_gem, yellow_gem, purple_gem];
+	var crystalValues = [];
+	//Function to assign a unique value to a crystal
+	var generateCrystalValue = function(){   
+		var isNewValue = true;
+		var value;
+		do{
+			value = Math.floor((Math.random() * 12) + 1); 
+		 	isNewValue = testUniqueCrystalValue(value);
+		}
+		while(isNewValue === false)
+		crystalValues.push(value);
+		return value;
+	
+	};
+	//Test if a value already assigned to crytsal
+	var testUniqueCrystalValue = function (value) {
+		var newValueTest = $.inArray(value, crystalValues);
+		if(newValueTest === -1){
+			return true;
+		}
+		else{
+			return false;
+		}
 
+	}
 
+	var generateTargetValue = function(){
+		return Math.floor((Math.random() * 101) + 19);
+	};
+
+	//The following functions relate to updating the game info on the user's screen
 	var writeTargetToScreen = function(){
-		$("#random-goal").html("<p>"+ targetValue + "</p>");
+		$("#random-goal").html(targetValue);
 	};
 
 	var writePlayerScoreToScreen = function(){
-		$("#score").html("<p>Your total score is: " + playerScore + "</p>");
+		$("#score").text("Your total score is: " + playerScore);
 	};
 
 	var writeWinLossToScreen = function(){
-		$("#win_lose").html("<p>Wins: " + win +"<br>Losses: " + loss + "</p>");
+		$("#win_lose").html("Wins: " + win +"<br>Losses: " + loss );
 	}
 
 	var resetGame = function(){
+		crystalValues = [];
 		$("img").each(function(index) {
 			$(this).data('value',generateCrystalValue());
 		});
@@ -46,19 +62,20 @@ $(document).ready(function(){
 		targetValue = generateTargetValue();
 		writeTargetToScreen();
 	};
-
+	//Get a target value for the game and put it in the DOM
 	var targetValue = generateTargetValue();
 	writeTargetToScreen();
 	writeWinLossToScreen();
 	
 	// Dynamically add crystals with a random value to document
 	for(var ii = 0; ii < gems_pic.length; ii++){
-		var crystHold = $('<div>');	
-		var img = $("<img width=150 height=100>");
+		var crystHold = $('<div>');	//crystHold is wrapper for imgs, allows me to scale image without pushing content
+		var img = $("<img>");
 		img.addClass("crystals-click");
-		img.attr('src', gems_pic[ii]);
+		img.attr('src', gems_pic[ii]); //Give each img the corresponding crystal picture
 		var val = generateCrystalValue();
 		img.data('value', val);
+		//Put crystal in the holder and append holder into crystal div
 		crystHold.append(img);
 		crystHold.addClass('crystal-holder');
 		$("#crystals").append(crystHold);
@@ -68,6 +85,7 @@ $(document).ready(function(){
 	$(".crystals-click").on('click', function(){
 		playerScore += ($(this).data('value'));
 		writePlayerScoreToScreen();
+		//Check to see if the user is at the value or if they've gone over
 		if(playerScore > targetValue){
 			loss++;
 			writeWinLossToScreen();
@@ -83,7 +101,7 @@ $(document).ready(function(){
 		}
 	});
  
- 	// Transition/Animation for mouse
+ 	// Change mouse and scale crystal when hovering over crystals
 	$(".crystals-click").hover(function(){
 			$('html,body').css('cursor' , 'pointer');
 			$(this).css('transform', 'scale(1.5,1.5)');
